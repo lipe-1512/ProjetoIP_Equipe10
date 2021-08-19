2cb6d4caef5db460a32a913d1d7240f3f464a6ad
Theo-HCP <86299369+Theo-HCP@users.noreply.github.com>
2021-08-19 14:42:54 -0300

    Add files via upload
    Módulos separados das classes do jogador/item e parede/salas

---
+++ Wall_Room.py
@@ -0,0 +1,98 @@
+import pygame
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+PURPLE = (255, 0, 255)
+YELLOW = (255, 255, 0)
+
+
+class Wall(pygame.sprite.Sprite):
+    """This class represents the bar at the bottom that the player controls """
+
+    def __init__(self, x, y, width, height, color):
+        """ Constructor function """
+
+        # Call the parent's constructor
+        super().__init__()
+
+        # Make a BLUE wall, of the size specified in the parameters
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+
+
+class Room(object):
+    """ Base class for all rooms. """
+
+    # Each room has a list of walls.
+    wall_list = None
+
+    def __init__(self):
+        """ Constructor, create our lists. """
+        self.wall_list = pygame.sprite.Group()
+        self.enemy_sprites = pygame.sprite.Group()
+
+
+class Room1(Room):
+    """This creates all the walls in room 1"""
+
+    def __init__(self):
+        super().__init__()
+        # Make the walls. (x_pos, y_pos, width, height)
+
+        # This is a list of walls. Each is in the form [x, y, width, height]
+        walls = [[0, 0, 20, 600, WHITE],
+                 [780, 0, 20, 600, WHITE],
+                 [20, 0, 760, 20, WHITE],
+                 [20, 580, 760, 20, WHITE],
+                 [390, 50, 20, 500, BLUE]
+                 ]
+
+        # Loop through the list. Create the wall, add it to the list
+        for item in walls:
+            wall = Wall(item[0], item[1], item[2], item[3], item[4])
+            self.wall_list.add(wall)
+
+
+class Room2(Room):
+    """This creates all the walls in room 2"""
+
+    def __init__(self):
+        super().__init__()
+
+        walls = [[0, 0, 20, 600, RED],
+                 [780, 0, 20, 600, RED],
+                 [20, 0, 760, 20, RED],
+                 [20, 580, 760, 20, RED]
+
+                 ]
+
+        for item in walls:
+            wall = Wall(item[0], item[1], item[2], item[3], item[4])
+            self.wall_list.add(wall)
+
+
+class Room3(Room):
+    """This creates all the walls in room 3"""
+
+    def __init__(self):
+        super().__init__()
+
+        walls = [[0, 0, 20, 600, PURPLE],
+                 [780, 0, 20, 600, PURPLE],
+                 [20, 0, 760, 20, PURPLE],
+                 [20, 580, 760, 20, PURPLE]
+                 ]
+
+        for item in walls:
+            wall = Wall(item[0], item[1], item[2], item[3], item[4])
+            self.wall_list.add(wall)
