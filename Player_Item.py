989a6e511aa1a66da8d4b65e6d7acd2972de5b45
LeoM0108 <69467961+LeoM0108@users.noreply.github.com>
2021-08-23 15:31:04 -0300

    Feature: Add end screens

--- Player_Item.py
+++ Player_Item.py
@@ -1,4 +1,5 @@
 import pygame
+from Labirinto import *
 
 pygame.init()
 
@@ -10,86 +11,30 @@ RED = (255, 0, 0)
 PURPLE = (255, 0, 255)
 YELLOW = (255, 255, 0)
 
-
-class Player(pygame.sprite.Sprite):
+swallows = pygame.sprite.Group()
+class Treasure(pygame.sprite.Sprite):
     """ This class represents the bar at the bottom that the
     player controls """
-
-    # Set speed vector
-    change_x = 0
-    change_y = 0
-
+ 
     def __init__(self, x, y):
         """ Constructor function """
-
         # Call the parent's constructor
         super().__init__()
+        #self.treasure_list = pygame.sprite.Group()
 
         # Set height, width
         self.image = pygame.Surface([15, 15])
-        self.image.fill(WHITE)
-
-        # Make our top-left corner the passed-in location.
-        self.rect = self.image.get_rect()
-        self.rect.y = y
-        self.rect.x = x
-
-    def changespeed(self, x, y):
-        """ Change the speed of the player. Called with a keypress. """
-        self.change_x += x
-        self.change_y += y
-
-    def move(self, walls):
-        """ Find a new position for the player """
-
-        # Move left/right
-        self.rect.x += self.change_x
-
-        # Did this update cause us to hit a wall?
-        block_hit_list = pygame.sprite.spritecollide(self, walls, False)
-        for block in block_hit_list:
-            # If we are moving right, set our right side to the left side of
-            # the item we hit
-            if self.change_x > 0:
-                self.rect.right = block.rect.left
-            else:
-                # Otherwise if we are moving left, do the opposite.
-                self.rect.left = block.rect.right
-
-        # Move up/down
-        self.rect.y += self.change_y
-
-        # Check and see if we hit anything
-        block_hit_list = pygame.sprite.spritecollide(self, walls, False)
-        for block in block_hit_list:
-
-            # Reset our position based on the top/bottom of the object.
-            if self.change_y > 0:
-                self.rect.bottom = block.rect.top
-            else:
-                self.rect.top = block.rect.bottom
-
-
-class Treasure(pygame.sprite.Sprite):
-    """ This class represents collectable items  """
-
-    def __init__(self, x, y):
-        """ Constructor function """
-        # Call the parent's constructor
-        super().__init__()
-        # self.treasure_list = pygame.sprite.Group()
-
-        # Set height, width
-        self.image = pygame.Surface([25, 25])
         self.image.fill(YELLOW)
-
+ 
         # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
 
+    
     def position(self, walls):
+
         # Check and see if we hit anything
-        block_hit_list = pygame.sprite.spritecollide(self, walls, False)
+        block_hit_list = pygame.sprite.spritecollide(self, walls,False)
         for block in block_hit_list:
             self.rect.top = block.rect.bottom
